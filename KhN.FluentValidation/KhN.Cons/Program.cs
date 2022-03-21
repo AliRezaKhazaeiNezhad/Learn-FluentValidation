381c1b5063d03ad61ec9c4658bfb8e9ac26a6669
AliReza <khazaeinezhadalireza@gmail.com>
2022-03-21 22:35:17 +0330

    ملزومات اولیه برای شروع آموزش افزوده شده است

--- KhN.FluentValidation/KhN.Cons/Program.cs
+++ KhN.FluentValidation/KhN.Cons/Program.cs
@@ -1,11 +1,27 @@
 ﻿
+using KhN.Domain.Models;
+
 namespace KhN.Cons 
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            User user = new User();
+
+            if (string.IsNullOrEmpty(user.FirstName))
+            {
+                Console.WriteLine("Please enter the FirstName");
+            }
+
+
+            if (string.IsNullOrEmpty(user.LastName))
+            {
+                Console.WriteLine("Please enter the LastName");
+            }
+
+
+            Console.WriteLine("Please [Enter] To Exit ...");
         }
     }
 }
---
+++ KhN.FluentValidation/KhN.Domain/Models/User.cs
@@ -0,0 +1,14 @@
+﻿
+namespace KhN.Domain.Models
+{
+    public class User : Object
+    {
+        public User() : base()
+        {
+
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
